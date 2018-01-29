94c92c103df35013b6713de0abd8b9824ee4b562
knightr6 <draftsman319@gmail.com>
2018-01-29 02:44:42 -0500

    Add files via upload

---
+++ Web_App/javascript/mobile.js
@@ -0,0 +1,16 @@
+
+
+function myFunction()	
+
+{	 	
+	alert("Dragon activity about your GPS location has been reported. Particular attention should be paid to open roadways with hot dark asphalt and updrafts and also ice-cream shops.");		
+}
+
+
+function jumpto(x)
+{
+	if (document.form2.jumpmenu.value != "null") 
+	{
+		document.location.href = x
+	}
+}
